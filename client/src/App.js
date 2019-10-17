8008dbc0227e004abe6653e5cafade93f692e12d
Droptime24 <you@example.com>
2019-10-17 14:38:38 -0600

    added articles page and user page for bolg posting

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Home from "./Pages/home";
 import Meetups from "./Pages/meetups";
 import Savings from "./Pages/savings";
 import Questions from "./Pages/questions";
+import Articles from "./Pages/articles";
+import User from "./Pages/user";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Route exact path="/meetups" component={Meetups} />
         <Route exact path="/savings" component={Savings} />
         <Route exact path="/questions" component={Questions} />
+        <Route exact path="/articles" component={Articles} />
+        <Route exact path="/user" component={User} />
       </Switch>
     </div>
   </Router>
