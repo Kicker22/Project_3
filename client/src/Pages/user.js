cf9c775c93e89cc75168a86296e17e4657c45151
Tyler Nichols <tegulator98@gmail.com>
2019-10-21 20:53:21 -0600

    User style finished.

--- client/src/Pages/user.js
+++ client/src/Pages/user.js
@@ -3,7 +3,7 @@
 import React, { Component } from "react";
 import BlogPost from "../Components/Form/user";
 import TopNavbar from "../Components/Navbar/TopNavbar";
-import { Container, Nav } from "react-bootstrap"
+import { Container, Nav, Card, Row, Button } from "react-bootstrap"
 export default class user extends Component {
   render() {
     return (
@@ -11,9 +11,34 @@ export default class user extends Component {
         <Container className = "text-center">
         <TopNavbar>
         </TopNavbar>
-        
         <BlogPost />
-        <Nav.Item>
+        </Container>
+        <Container className = "text-center">
+        <h1 className="text-center m-auto p-3 text-info">Past Posts</h1>
+        <Row>
+              <Card className="content-card" style={{ width: "19rem" }}>
+                <Card.Body>
+                  <Card.Title>Past Post</Card.Title>
+                  <Card.Text>Auto filled blog cards</Card.Text>
+                  <Button variant="primary">Go To</Button>
+                </Card.Body>
+              </Card>
+              <Card className="content-card" style={{ width: "19rem" }}>
+                <Card.Body>
+                  <Card.Title>Past Post</Card.Title>
+                  <Card.Text>Auto filled blog cards</Card.Text>
+                  <Button variant="primary">Go To</Button>
+                </Card.Body>
+              </Card>
+              <Card className="content-card" style={{ width: "19rem" }}>
+                <Card.Body>
+                  <Card.Title>Past Post</Card.Title>
+                  <Card.Text>Auto filled blog cards</Card.Text>
+                  <Button variant="primary">Go To</Button>
+                </Card.Body>
+              </Card>
+            </Row>
+            <Nav.Item>
             <Nav.Link className="text-info py-4" href="/">
               Home
             </Nav.Link>
