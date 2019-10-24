7b78684fdcd94d327d9e94112c5d7969eed92090
Matt <mattsully01@gmail.com>
2019-10-23 20:01:59 -0600

    test

--- server.js
+++ server.js
@@ -33,19 +33,7 @@ app.use(function(err, req, res, next) {
 
 // connection port
 const PORT = process.env.PORT || 5000;
-// cors_proxy.createServer({
-//     originWhitelist: [`origin: process.env.ORIGIN_URL || "http://localhost",
-//     optionsSuccessStatus: 200`], // Allow all origins
-//     requireHeader: ['origin', 'x-requested-with'],
-//     removeHeaders: ['cookie', 'cookie2'],
-//     AccessControlAllowOrigin: ['*']
-// });
-// .listen(PORT, 'http://localhost', function() {
-//     console.log('Running CORS Anywhere on ' + 'http://localhost + ':' + port);
-// });
 
-// DB Config
-// const db = require('./keys').MongoURI;
 var db = process.env.MONGODB_URI || "mongodb://localhost/PROJECT-3";
 
 // Connect to Mongo
@@ -58,17 +46,10 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.use(express.static("/app/public"));
 }
-// app.get('*', (request, response) => {
-// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
-// serving static test files
-// app.use(express.static(__dirname + './client/public'));
+
 app.use(express.static(__dirname + "/app/public/"));
 require("./app/routes/htmlRoute")(app);
-// end of test files
-// app.use("/cors/*", function(req, res) {
-//     req.pipe(request(req.params[0])).pipe(res);
-//   });
+
 
 app.listen(PORT, () =>
   console.log(`server started on http://locahost: ${PORT}`)
