800550f974102fd6e4bd1f8ac027a6427f0f7f0b
Tyler Nichols <tegulator98@gmail.com>
2019-10-23 19:04:20 -0600

    better styling for articles.

--- client/src/Pages/articles.js
+++ client/src/Pages/articles.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Media from "react-bootstrap/Media";
-import { Container, Nav } from "react-bootstrap";
-import TopNavbar from "../Components/Navbar/TopNavbar"
+import { Container, Nav, Row } from "react-bootstrap";
+import TopNavbar from "../Components/Navbar/TopNavbar";
 
 export default class articles extends Component {
   render() {
@@ -11,12 +11,13 @@ export default class articles extends Component {
           
         </TopNavbar>
         <Container className="text-center">
+      <Row>
           <Media className="mt-4">
             <img
               width={64}
               height={64}
-              className="mr-3"
-              src="holder.js/64x64"
+              className="mr-10"
+              src="http://theblogofteresa.com/wp-content/uploads/2013/10/heart_red_mom.png"
               alt="Generic placeholder"
             />
             <Media.Body>
@@ -28,13 +29,16 @@ export default class articles extends Component {
                 condimentum nunc ac nisi vulputate fringilla. Donec lacinia
                 congue felis in faucibus.
               </p>
-
+              </Media.Body>
+          </Media>
+              </Row>
+              <Row>
               <Media>
                 <img
                   width={64}
                   height={64}
-                  className="mr-3"
-                  src="holder.js/171x180"
+                  className="mr-10"
+                  src="https://previews.123rf.com/images/baldyrgan/baldyrgan1504/baldyrgan150400020/38998743-mother-and-son-stylized-vector-silhouette-outlined-sketch-of-mom-and-child.jpg"
                   rounded
                   alt="Generic placeholder"
                 />
@@ -49,8 +53,7 @@ export default class articles extends Component {
                   </p>
                 </Media.Body>
               </Media>
-            </Media.Body>
-          </Media>
+              </Row>
         </Container>
         <Nav.Item className = "text-center">
             <Nav.Link className="text-info py-4" href="/">
