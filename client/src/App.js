e145a1d110ef644c0555b4d50c9041696fac868c
Matt <mattsully01@gmail.com>
2019-10-25 00:45:18 -0600

    adding changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import './App.css'
 import Home from "./Pages/home";
-import Meetups from "./Pages/meetups";
 import Savings from "./Pages/savings";
 import Questions from "./Pages/questions";
 import Articles from "./Pages/articles";
@@ -18,7 +17,6 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/meetups" component={Meetups} />
         <Route exact path="/savings" component={Savings} />
         <Route exact path="/questions" component={Questions} />
         <Route exact path="/articles" component={Articles} />
