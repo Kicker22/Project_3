d3658a15a2495a7736594d094fbab7db3ffad5b0
Matt <mattsully01@gmail.com>
2019-10-25 00:31:13 -0600

    added build file

---
+++ client/build/precache-manifest.b06f1d9d1c5589c39561ea0d55282a7f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275980a43c8781cfaca02813b21ccddc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3ab258ed971ab1bf81e",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "76487b88aa10151b678f",
+    "url": "/static/css/main.21832b02.chunk.css"
+  },
+  {
+    "revision": "c3ab258ed971ab1bf81e",
+    "url": "/static/js/2.6996d967.chunk.js"
+  },
+  {
+    "revision": "76487b88aa10151b678f",
+    "url": "/static/js/main.7daf127d.chunk.js"
+  },
+  {
+    "revision": "db81880d187076eba105",
+    "url": "/static/js/runtime-main.b3f7267f.js"
+  },
+  {
+    "revision": "59d200de016bc11a0476f6bc3ed6b1d7",
+    "url": "/static/media/boards.59d200de.png"
+  },
+  {
+    "revision": "544f2557280105cdafbcd3c8c822dc07",
+    "url": "/static/media/meetup.544f2557.png"
+  },
+  {
+    "revision": "1be652efc89af06bdf08e567bb6a4d49",
+    "url": "/static/media/questions.1be652ef.png"
+  },
+  {
+    "revision": "798e4583af8e62b614a4ab6b68746de5",
+    "url": "/static/media/savings.798e4583.png"
+  },
+  {
+    "revision": "d09534cdda18fd1831ffffe3a17e610c",
+    "url": "/static/media/slide1.d09534cd.png"
+  },
+  {
+    "revision": "56d0207f2ca7f8c34b5a01569846d75d",
+    "url": "/static/media/slide2.56d0207f.png"
+  },
+  {
+    "revision": "8e2e90a8d6fc233ebd1884c8c16318f5",
+    "url": "/static/media/slide3.8e2e90a8.png"
+  }
+]);
