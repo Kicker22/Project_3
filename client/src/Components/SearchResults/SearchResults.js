4cd21ff79375503b836d732e51fa9e6f12630cd4
Droptime24 <you@example.com>
2019-10-24 21:51:55 -0600

    updates for clien, re added meetups and api

--- client/src/Components/SearchResults/SearchResults.js
+++ client/src/Components/SearchResults/SearchResults.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { Card, Container } from "react-bootstrap";
 
-function SearchResults (props) {
+function SearchResults(props) {
   return (
-    <div className="text-center">
-      {/* <img alt={props.title} className="img-fluid" src={props.src} style={{ margin: "0 auto" }} /> */}
-      <h3>Name of Group: {props.name}</h3>
-      <h3>Number of Groups: {props.group_count}</h3>
-      <h3>Total Members: {props.member_count}</h3>
-      <p>Description: {props.description}</p>
-    </div>
+    <Container>
+      <Card className="m-4 p-2 bg-transparent">
+        <div className="text-justify bg-transparent">
+          <ul>
+            <li>
+              <h4>Group Name: {props.name}</h4>
+            </li>
+            <li>
+              <h4>Number of Groups: {props.group_count}</h4>
+            </li>
+            <li>
+              <h4>Members: {props.member_count}</h4>
+            </li>
+          </ul>
+          
+          <h4>Description:</h4> <p>{props.description}</p>
+
+        </div>
+      </Card>
+    </Container>
   );
 }
 
--- client/src/Pages/meetups.js
+++ client/src/Pages/meetups.js
@@ -44,50 +44,49 @@ export default class meetups extends Component {
   render() {
     return (
       <div>
-        <TopNavbar/>
-      <Container className="text-center mt-4">
-        <br />
-        <br />
-        <Image src={Meetup} fluid />
-        <br />
-        <Jumbotron className="content-center mt-4 bg-transparent">
-          <h2 className="flex-center">Find, Connect, and Grow</h2>
-          <Row>
-            <Col size="md-8">
-              <Card
-                heading={this.state.result.id || "Search for a Topic to Join"}
-              >
-                {this.state.result.map( result => (
-                  <SearchResults 
-                    id={result.id}
-                    name={result.name}
-                    urlkey={result.urlkey}
-                    group_count={result.group_count}
-                    description={result.description}
-                    lang={result.lang}
-                  />
-                ))} {this.state.result.length === 0 && (
-                  <h3>No Results to Display</h3>
-                )}
-              </Card>
-            </Col>
-            <Col size="md-4">
-              <Card heading="Search">
-                <SearchForm
-                  value={this.state.search}
-                  handleInputChange={this.handleInputChange}
-                  handleFormSubmit={this.handleFormSubmit}
-                />
-              </Card>
-            </Col>
-          </Row>
-        </Jumbotron>
-        <Nav.Item>
-          <Nav.Link className="text-info py-4" href="/">
-            Home
-          </Nav.Link>
-        </Nav.Item>
-      </Container>
+        <TopNavbar />
+        <Container className="text-center mt-4">
+          <br />
+          <br />
+          <Image src={Meetup} fluid />
+          <br />
+          <Jumbotron className="content-center mt-4 bg-transparent">
+            <h2 className="flex-center">Find, Connect, and Grow</h2>
+            <Row>
+                <Col className="col-3">
+                  <Card className="m-auto" heading="Search">
+                    <SearchForm
+                      value={this.state.search}
+                      handleInputChange={this.handleInputChange}
+                      handleFormSubmit={this.handleFormSubmit}
+                    />
+                  </Card>
+                </Col>
+              <Col className="col-md-9">
+                <Card heading={this.state.result.id || "Search Results"}>
+                  {this.state.result.map(result => (
+                    <SearchResults
+                      id={result.id}
+                      name={result.name}
+                      urlkey={result.urlkey}
+                      group_count={result.group_count}
+                      description={result.description}
+                      lang={result.lang}
+                    />
+                  ))}
+                  {this.state.result.length === 0 && (
+                    <h3>No Results to Display</h3>
+                  )}
+                </Card>
+              </Col>
+            </Row>
+          </Jumbotron>
+          <Nav.Item>
+            <Nav.Link className="text-info py-4" href="/">
+              Home
+            </Nav.Link>
+          </Nav.Item>
+        </Container>
       </div>
     );
   }
