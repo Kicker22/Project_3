5453f173b9958572ebcb7ac49985f14a9e77b790
mworth <mikeworthen55@gmail.com>
2019-10-26 01:11:41 -0600

    added explanation for the site and some spacing.

--- client/src/Pages/home.js
+++ client/src/Pages/home.js
@@ -32,6 +32,7 @@ export default class index extends Component {
         <h1 className="title text-center pt-2">Mommie Blog</h1>
           <br />
         <Container className="m-auto text-center">
+        <h3 className="">Mommie Blog is a one stop shop for moms to come and converse with other moms. Post your own tips and tricks for everyday life as a mom and ask questions to recieve feedback. You can also post any sales that you might have found at your local grocery store and find meet ups with other moms in your area.  </h3>
           <Nav className="justify-content-center my-4" activeKey="/home">
             <Nav.Item></Nav.Item>
             <Nav.Item>
@@ -44,22 +45,19 @@ export default class index extends Component {
             <Carousel.Item>
               <img className="d-block w-100" src={Slide1} alt="First slide" />
               <Carousel.Caption className="text-light">
-                <h3>Fun At home</h3>
-                <p>Find Something Fun at Home Here!</p>
+                
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img className="d-block w-100" src={Slide2} alt="Second slide" />
               <Carousel.Caption className="text-dark">
-                <h3>Budgeting</h3>
-                <p>Find ways to save money!</p>
+              
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
               <img className="d-block w-100" src={Slide3} alt="Third slide" />
               <Carousel.Caption className="text-secondary">
-                <h3>Mommy Meetups</h3>
-                <p>Find your get together mommy group!</p>
+            
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
@@ -69,7 +67,7 @@ export default class index extends Component {
               <Card className="bg-transparent 100px100">
                 <Card.Img src={Questions} alt="Card image" />
                 <Card.Footer>
-                  <Button variant="outline-dark" href="/questions">
+                  <Button variant="outline-dark" onClick={() => window.location.href="/questions"}>
                     VIEW
                   </Button>
                 </Card.Footer>
@@ -79,7 +77,7 @@ export default class index extends Component {
               <Card className="bg-transparent 100px100">
                 <Card.Img src={Savings} alt="Card image" />
                 <Card.Footer>
-                  <Button variant="outline-dark" href="/savings">
+                  <Button variant="outline-dark" onClick={() => window.location.href="/savings"}>
                     VIEW
                   </Button>
                 </Card.Footer>
@@ -89,7 +87,7 @@ export default class index extends Component {
               <Card className="bg-transparent 100px100">
                 <Card.Img src={Meetup} alt="Card image" />
                 <Card.Footer>
-                  <Button variant="outline-dark" href="/meetups">
+                  <Button variant="outline-dark" onClick={() => window.location.href="/meetups"}>
                     VIEW
                   </Button>
                 </Card.Footer>
