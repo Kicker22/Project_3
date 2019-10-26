ff9a11559198dac8b652327e5cceb28a24d8ed71
Tyler Nichols <tegulator98@gmail.com>
2019-10-25 22:16:24 -0600

    possible 404 fix

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ mongoose
   .catch(err => console.log(err));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.use(express.static("/app/public"));
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
 }
 
 app.use(express.static(__dirname + "/app/public/"));
